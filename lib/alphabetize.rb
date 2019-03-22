87038c11fa66e0f8ad6570c2abd07fd72dc9ace0
Kat B <katreesebarnes@gmail.com>
2019-03-22 22:31:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 require "pry"
 def alphabetize(arr)
-  
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
   word.split('').map do |letter|
     ESPERANTO_ALPHABET.index(letter)
